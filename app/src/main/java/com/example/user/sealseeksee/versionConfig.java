ee7884c374bc479b69dd01c3e44e5e9aad8b2e87
david-igaw <david.park@igaworks.com>
2019-01-25 15:00:46 +0900

    version update

--- app/src/main/java/com/example/user/sealseeksee/versionConfig.java
+++ app/src/main/java/com/example/user/sealseeksee/versionConfig.java
@@ -2,7 +2,7 @@ package com.example.user.sealseeksee;
 
 public class versionConfig
 {
-    public static String version = "1.1.5";
+    public static String version = "1.1.6";
     public static String lastModified = "19-01-25";
 
 
