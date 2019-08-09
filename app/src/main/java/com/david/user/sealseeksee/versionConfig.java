3dd631d3edba06b4efbc61885a69eb5b993752b8
박홍욱 <david.park@igaworks.com>
2019-08-09 18:03:14 +0900

    Release 1.3.1 : gif 색상 동기화 , 쪽지 찾기 버튼 색상 변경

--- app/src/main/java/com/david/user/sealseeksee/versionConfig.java
+++ app/src/main/java/com/david/user/sealseeksee/versionConfig.java
@@ -2,8 +2,8 @@ package com.david.user.sealseeksee;
 
 public class versionConfig
 {
-    public static String version = "1.2.2";
-    public static String lastModified = "19-03-26";
+    public static String version = "1.3.1";
+    public static String lastModified = "19-08-10";
 
 
     /*
